2428a137a808a47e95e60e8b994565fe38b462f0
Ana Clara Rodrigues da Silva <claraonel@outlook.com>
2026-06-09 14:09:01 -0300

    controller fornecedor

--- BuildUp/obj/Debug/net9.0/BuildUp.AssemblyInfo.cs
+++ BuildUp/obj/Debug/net9.0/BuildUp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c1619b7194aac60a92dd17664585111825f58dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6417e909c16a806d7e1f343629cf8295af68b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
