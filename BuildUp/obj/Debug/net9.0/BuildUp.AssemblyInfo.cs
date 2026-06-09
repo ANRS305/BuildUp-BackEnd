fe8c65a35f68e38ed797cb4de09e8e8ef2c733c1
Ana Clara Rodrigues da Silva <claraonel@outlook.com>
2026-06-09 16:24:06 -0300

    .

--- BuildUp/obj/Debug/net9.0/BuildUp.AssemblyInfo.cs
+++ BuildUp/obj/Debug/net9.0/BuildUp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856f3691c2e32879cb4d5da658056733341aa2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cf6ed8c27b7e19398221dc5562028f752e7d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
